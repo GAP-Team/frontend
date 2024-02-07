2eafd14ee1316f68a1cc329b861123b412080c5e
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-02-07 07:38:45 +0100

    GP-86 Offer section implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import BlogSection from "@/components/landingpage/blog";
-import Features from '@/components/landingpage/features';
+import Features from "@/components/landingpage/features";
+import OfferSection from "@/components/landingpage/offer_section";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Features/>
+      <Features />
+      <OfferSection />
       <BlogSection />
     </main>
   );
