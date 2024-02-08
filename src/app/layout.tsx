7db84b8a4cad76de997901019111f843199e3d38
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-02-08 06:26:04 +0100

    GP-87 Testimonial Section implemented and flowbite installed along with its configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ThemeModeScript } from "flowbite-react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <ThemeModeScript />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Features from "@/components/landingpage/features";
 
 import Image from "next/image";
 import FunctionSection from "./../components/landingpage/function_section";
+import Testimonials from "@/components/landingpage/testimonials";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      <Testimonials />
       <FunctionSection />
       <Features />
       <BlogSection />
