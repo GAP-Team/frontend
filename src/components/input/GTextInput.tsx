4ee5081b666943d0d18fe050f4bde9d6e89f49b0
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-06-19 03:58:50 +0200

    adding password feild in registration form

--- src/components/input/GTextInput.tsx
+++ src/components/input/GTextInput.tsx
@@ -1,43 +1,16 @@
 import React, { FocusEventHandler } from "react";
-import TextField from "@mui/material/TextField";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
 
-interface TextInputProps {
-  placeholder?: string;
-  value?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  error?: boolean | undefined;
-  id?: string;
-  name?: string;
-  label?: string;
-  onBlur?: FocusEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined;
-  helperText?: string;
-}
-
-const GTextInput: React.FC<TextInputProps> = ({
-  placeholder,
-  value,
-  onChange,
-  error,
-  id,
-  name,
-  label,
-  onBlur,
-  helperText,
+const GTextInput: React.FC<TextFieldProps> = ({
+  variant = "outlined",
+  ...otherProps
 }) => {
   return (
     <TextField
       InputProps={{ sx: { borderRadius: "0.5rem" } }}
-      id={id}
-      name={name}
       fullWidth
       variant="outlined"
-      value={value}
-      placeholder={placeholder}
-      error={error}
-      label={label}
-      onBlur={onBlur}
-      helperText={helperText}
-      onChange={onChange}
+      {...otherProps}
     />
   );
 };
