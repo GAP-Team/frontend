d7fd062b2344feef6b02972148a97c791df05512
rihabalyasiri <rihab.al-yasiri@web.de>
2024-07-06 13:06:22 +0200

    solve the conflict

--- src/screens/dashboard/buildings/add_building_form/BuildingDocumentation.tsx
+++ src/screens/dashboard/buildings/add_building_form/BuildingDocumentation.tsx
@@ -22,62 +22,7 @@ const BuildingDocumentation = ({ formik }: { formik?: any }) => {
       sx={{ p: 1, width: "auto", marginLeft: "1.5rem" }}
     >
       <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="gsub" color="gray.500">
-            DOKUMENTE
-          </Typography>
-          <UploadMultiButton
-            id="constructionDocs"
-            name="constructionDocs"
-            value={formik.values.constructionDocs}
-            // onChange={(ev:any) => { handleConstructionS3Upload(ev) }}
-            onChange={formik.handleChange}
-            error={formik.touched.constructionDocs && Boolean(formik.errors.constructionDocs)}
-            helperText={formik.touched.constructionDocs && formik.errors.constructionDocs}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="gsub" color="gray.500">
-            GRUNDRISSE
-          </Typography>
-          <UploadMultiButton
-            id="floorplanDocs"
-            name="floorplanDocs"
-            value={formik.values.floorplanDocs}
-            onChange={formik.handleChange}
-            error={formik.touched.floorplanDocs && Boolean(formik.errors.floorplanDocs)}
-            helperText={formik.touched.floorplanDocs && formik.errors.floorplanDocs}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="gsub" color="gray.500">
-            SONSTIGE DOKUMENTE
-          </Typography>
-          <UploadMultiButton
-            id="otherDocs"
-            name="otherDocs"
-            value={formik.values.otherDocs}
-            onChange={formik.handleChange}
-            error={formik.touched.otherDocs && Boolean(formik.errors.otherDocs)}
-            helperText={formik.touched.otherDocs && formik.errors.otherDocs}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="gsub" color="gray.500">
-            SERVER-LINK HINZUFPGEN
-          </Typography>
-          <GTextInput
-            placeholder="Geben Sie Ihre Link, e.g https://www.icloud.com/notes/xxxx"
-            id="serverLink"
-            name="serverLink"
-            value={formik?.values.serverLink}
-            onChange={formik?.handleChange}
-            onBlur={formik?.handleBlur}
-            error={
-              formik?.touched?.serverLink && Boolean(formik?.errors?.serverLink)
-            }
-            helperText={formik?.touched?.serverLink && formik?.errors?.serverLink}
-          />
+      
           <FormControl sx={{ display: "block" }}>
             <RadioGroup
               id="documentChoice"
@@ -204,7 +149,6 @@ const BuildingDocumentation = ({ formik }: { formik?: any }) => {
             />
           </Grid>
         )}
-      </Grid>
     </Box>
   );
 };
