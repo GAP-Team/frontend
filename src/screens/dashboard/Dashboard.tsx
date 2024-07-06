d7f8bc65a33965ab529fb666d41a1d53056d8737
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-07-06 10:54:52 +0200

    GP-277 change of title to neue Ausschreibung in sidebar item

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -21,7 +21,7 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ overrideComponent  }) => {
   const sidebarItems: SidebarItem[] = [
     { id:0, icon: LuLayoutDashboard, text: "Dashboard", component:<RealEstateUser/> },
-    { id:1, icon: CgNotes, text: "Ausschreibungen", subItems: [{ id: 10, text: "Alle Ausschreibungen", component:<Tenders /> }, { id: 11, text: "Neue hinzufügen", component: <NewTender/>}]},
+    { id:1, icon: CgNotes, text: "Ausschreibungen", subItems: [{ id: 10, text: "Alle Ausschreibungen", component:<Tenders /> }, { id: 11, text: "Neue Ausschreibung", component: <NewTender/>}]},
     { id:2, icon: MdOutlineDoorSliding, text: "Anlagen" },
     { id: 3, icon: MdOutlineAddHomeWork, text: "Gebäude", subItems: [{ id: 30, text: "Alle Gebäude", component:<Buildings /> }, { id: 31, text: "Gebäude hinzufügen", component: <NewBuilding/>}] },
     { id:4, icon: MdOutlineNoteAlt, text: "Aufträge"},
