79efd065d9c53f5044279d395edb2e218958b056
SudiptoVE <sudipto@gap-pruefen.de>
2024-07-16 11:38:55 +0530

    contact person is optinal misc chnages

--- src/screens/dashboard/buildings/add_building_form/BuildingInformation.tsx
+++ src/screens/dashboard/buildings/add_building_form/BuildingInformation.tsx
@@ -132,7 +132,6 @@ const BuildingInformation = ({ formik }: { formik?: any }) => {
           />
         </Grid>
         <Grid item xs={12} sm={11}>
-          {/* <LabelWithAsterisk></LabelWithAsterisk> */}
           <Typography variant="gsub" color="gray.500"> ANSPRECHPARTNER HINZUFÜGEN</Typography>
           <Autocomplete
             multiple
