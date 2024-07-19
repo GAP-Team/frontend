8ef7f20a62ce83a8a16254acc110ddb0f7caa14e
SudiptoVE <sudipto@gap-pruefen.de>
2024-07-19 18:27:54 +0530

    removing logger messages

--- src/screens/dashboard/buildings/add_building_form/NewBuilding.tsx
+++ src/screens/dashboard/buildings/add_building_form/NewBuilding.tsx
@@ -25,10 +25,6 @@ const NewBuilding = () => {
 
   const router = useRouter();
   const logger = getLogger("new-building");
-  
-  logger.error("------------------------- > a error message from Home");
-  logger.debug(".....................> a debug message from Home");
-  logger.info("==========================> a info message from Home");
 
   const steps: ActiveStepItem[] = [
     {
