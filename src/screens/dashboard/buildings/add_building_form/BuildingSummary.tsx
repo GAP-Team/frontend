3e362c8fbf93f570dfcda5c5bec031313861bbd3
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-07-23 21:20:28 +0200

    Added check if there are contact person then display the summary cell otherwise not

--- src/screens/dashboard/buildings/add_building_form/BuildingSummary.tsx
+++ src/screens/dashboard/buildings/add_building_form/BuildingSummary.tsx
@@ -49,9 +49,11 @@ const BuildingSummary = ({setActiveStep,steps}:BuildingSummaryProps) => {
         <Grid item xs={12}>
           <SummarySection title="Objektanschrift" details={updatedAddress} setActiveStep={()=>setActiveStep(steps[1])}/>
         </Grid>
-        <Grid item xs={6}>
-          <SummarySection title="Ansprechpartner" details={updatedContactPersonList} setActiveStep={()=>setActiveStep(steps[0])} />
-        </Grid>
+        {updatedContactPersonList.length > 0 && (
+          <Grid item xs={6}>
+           <SummarySection title="Ansprechpartner" details={updatedContactPersonList} setActiveStep={()=>setActiveStep(steps[0])} />
+          </Grid>
+        )}
         {updatedDocList.length > 0 && (
           <Grid item xs={6}>
             <SummarySection title="Bauunterlagen" details={updatedDocList} setActiveStep={() => setActiveStep(steps[2])} />
