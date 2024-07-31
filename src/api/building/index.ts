f810062f894a9d0dd92feef3f90a5a26e9626e56
SudiptoVE <sudipto@gap-pruefen.de>
2024-07-31 13:01:08 +0530

    get and show user cities and states

--- src/api/building/index.ts
+++ src/api/building/index.ts
@@ -2,9 +2,9 @@ import api from "../axios";
 
 const buildingAPIs = {
 
-    /* Building Routes */
+    /* Building Routes */ 
     create: (data: any) => api.post('/buildings/create', data),
-  
+    getUserStatesCities: (id: string) => api.get(`/buildings/get-user-state-city/${id}`),
 }
 
 export default buildingAPIs;
