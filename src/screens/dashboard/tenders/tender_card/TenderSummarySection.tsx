2ca8fe4fbc5ff71d0e3e1e323b7decc36bea7200
rihabalyasiri <rihab.al-yasiri@web.de>
2024-08-31 17:10:49 +0200

    create a similar label without icon and change the type of label

--- src/screens/dashboard/tenders/tender_card/TenderSummarySection.tsx
+++ src/screens/dashboard/tenders/tender_card/TenderSummarySection.tsx
@@ -7,23 +7,10 @@ import Image from "next/image";
 import stepperHorizontal from "../../../../../public/icons/stepper-horizontal.svg";
 import Grid from "@mui/material/Grid";
 import { MdOutlineEdit } from "react-icons/md";
-import LabeledTextWithIcon from "@/components/label/LabelTextWithIcon";
 import Divider from "@mui/material/Divider";
 import GButton from "@/components/button/GButton";
-
-const summaryData = [
-  { label: "Name des Auftraggebers", value: "Fire Service GmbH" },
-  { label: "Name der Ausschreibung", value: "Fire Service GmbH" },
-  { label: "Ausschreibungsart", value: "Handwerker" },
-  { label: "Ausschreibungstyp", value: "SV-Begleitung" },
-  { label: "Objekt", value: "Handwerker" },
-  { label: "Anlage", value: "Handwerker" },
-  { label: "Anlagetyp", value: "Handwerker" },
-  { label: "Dringlichkeit", value: "Eröffnungstermin" },
-  { label: "Zeitfenster available", value: "14. Jan 2024 - 28 Mar. 2025" },
-  { label: "Detailbeschreibung", value: "Klicken Sie hier, um zu sehen" },
-  { label: "Dokumente", value: "Vor Ort zur Verfügung stellen" },
-];
+import { dummySummaryData } from "../../../../utils/Constants";
+import LabelText from "@/components/label/LabelText";
 
 const TenderSummarySection = () => {
   return (
@@ -40,16 +27,12 @@ const TenderSummarySection = () => {
         />
       </Box>
       <Grid container spacing={2} marginLeft={1}>
-        {summaryData.map((item, index) => (
+        {dummySummaryData.map((item, index) => (
           <Grid item xs={6} key={index} paddingBottom={2}>
-            <LabeledTextWithIcon
+            <LabelText
               text={item.label}
-              Icon={MdOutlineEdit}
-              iconColor="#22A7F1"
-              iconSize="1.4rem"
               fontSize="1.2rem"
               textColor="blue.main"
-              iconMarginLeft="0.8rem"
             />
             <Typography variant="bodylr" mt="0.2rem">
               {item.value}
