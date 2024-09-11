0445bdc273c5c06fc417dafa1708db4bfb77796a
rihabalyasiri <rihab.al-yasiri@web.de>
2024-09-11 10:47:51 +0200

    deleting the old dashboard structure

--- src/app/dashboard/tenders/page.tsx
+++
@@ -1,6 +0,0 @@
-import Dashboard from "@/screens/dashboard/Dashboard";
-import Tenders from "@/screens/dashboard/tenders/Tenders";
-
-export default function TendersPage() {
-  return <Dashboard overrideComponent={<Tenders />} />;
-}
