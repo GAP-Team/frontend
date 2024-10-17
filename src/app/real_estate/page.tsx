33e2de26dd2921620903eaa5f575b76c2bbdfec4
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-10-17 03:11:06 +0200

    prettier formated

--- src/app/real_estate/page.tsx
+++ src/app/real_estate/page.tsx
@@ -81,7 +81,9 @@ const sidebarItems: SidebarItem[] = [
 const RealStateUserLayout: React.FC<any> = ({ children }) => {
   const router = useRouter();
   const pathname = usePathname();
-  const [selected, setSelected] = useState<SidebarItem | SubItem>(sidebarItems[0]);
+  const [selected, setSelected] = useState<SidebarItem | SubItem>(
+    sidebarItems[0]
+  );
 
   useEffect(() => {
     const matchSidebarItem = () => {
