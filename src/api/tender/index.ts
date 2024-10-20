73e65f651736d6e1bc1f2ad5a63c7e9540fd7579
rihabalyasiri <rihab.al-yasiri@web.de>
2024-10-20 12:16:10 +0200

    test fetching tenders

--- src/api/tender/index.ts
+++ src/api/tender/index.ts
@@ -3,6 +3,7 @@ import api from "../axios";
 const tenderAPIs = {
   /* Tender Routes */
   create: (data: any): any => api.post("/tenders/create", data),
+  getTenders: (userId: string): any => api.get(`/tenders/users/${userId}`),
 };
 
 export default tenderAPIs;
