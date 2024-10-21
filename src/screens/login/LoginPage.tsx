a26f4c87b7bae5a4009d550ec849930a24a87683
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-10-21 04:23:59 +0200

    formated code

--- src/screens/login/LoginPage.tsx
+++ src/screens/login/LoginPage.tsx
@@ -60,10 +60,10 @@ export default function LoginPage(): JSX.Element {
 
   const handleChange =
     (field: string) =>
-      (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setLoginError(null);
-        formik.setFieldValue(field, e.target.value);
-      };
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
+      setLoginError(null);
+      formik.setFieldValue(field, e.target.value);
+    };
 
   return (
     <Grid container component="main" sx={styles.mainContainer}>
