7be2a2ccde1f93b072b0387c2d5cee91edefd8ce
Rihab <rihab.al-yasiri@adesso.de>
2024-10-22 21:28:25 +0200

    old field no need

--- src/screens/dashboard/buildings/building_card/types.tsx
+++ src/screens/dashboard/buildings/building_card/types.tsx
@@ -15,7 +15,6 @@ export interface Building {
     zip: string;
   };
   area: number; // Area in square meters
-  filesNames: string[] | null | undefined;
   documents: [
     {
       name: string;
