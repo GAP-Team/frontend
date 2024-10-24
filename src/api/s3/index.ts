cd1afa75d6f2a9be08d5dd3fda9b90a94699329b
SudiptoVE <sudipto@gap-pruefen.de>
2024-10-24 10:28:51 +0530

    refactoring Endpoint

--- src/api/s3/index.ts
+++ src/api/s3/index.ts
@@ -2,12 +2,12 @@ import api from "../axios";
 
 const s3APIs = {
   /* S3 Routes */
-  upload: (data: any): any => api.post("/files/upload", data),
+  upload: (data: any): any => api.post("/files", data),
   getFile: (key: string): any =>
-    api.get(`/files/download/${key}`, {
+    api.get(`/files/${key}`, {
       responseType: "blob",
     }),
-  delete: (key: any): any => api.delete(`/files/delete/${key}`),
+  delete: (key: any): any => api.delete(`/files/${key}`),
 };
 
 export default s3APIs;
