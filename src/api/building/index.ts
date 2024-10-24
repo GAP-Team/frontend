10e4d80c150f242192d2965fd38c747fee7e8370
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-10-24 02:12:34 +0200

    Gp-570 building endpoint refactoring changes

--- src/api/building/index.ts
+++ src/api/building/index.ts
@@ -2,7 +2,7 @@ import api from "../axios";
 
 const buildingAPIs = {
   /* Building Routes */
-  create: (data: any): any => api.post("/buildings/create", data),
+  create: (data: any): any => api.post("/buildings", data),
   update: (buildingId: any, data: any): any =>
     api.put(`/buildings/${buildingId}`, data),
   getUserStatesCitiesFacilityTypes: (id: string): any =>
