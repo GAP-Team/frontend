60c53b605ed9c47246d2e052a9db468aeab6aef9
rihabalyasiri <rihab.al-yasiri@web.de>
2024-11-10 18:11:58 +0100

    these two endpoints will not be used from frontend

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,10 +2,8 @@ import api from "../axios";
 
 const userAPIs = {
   /* User Registration Routes */
-  getAllUser: (): any => api.get("/users"),
   register: (data: any): any => api.post("/users", data),
   getUserDataById: (id: any): any => api.get(`/users/${id}`),
-  getUserData: (data: any): any => api.post("/users/email", data),
   verifyEmail: (data: any): any => api.post("/users/verify-user-token", data),
   updateUser: (id: string, data: any): any => api.patch(`/users/${id}`, data),
   sendVerificationEmail: (data: any): any => api.post("/emails/send", data),
