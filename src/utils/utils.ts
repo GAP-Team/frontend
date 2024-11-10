a44275b7821e3f8e62707e5173057b331c26569f
rihabalyasiri <rihab.al-yasiri@web.de>
2024-11-10 19:33:57 +0100

    text typo UX

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 export const getTenderStatusStyle: {
   [key: string]: { bgcolor: string; color: string; title: string };
 } = {
-  OPEN: { bgcolor: "#E7E0FF", color: "#582EFF", title: "öffen" },
+  OPEN: { bgcolor: "#E7E0FF", color: "#582EFF", title: "offen" },
   DONE: { bgcolor: "#E5F5FA", color: "#22A7F1", title: "abgeschlossen" },
   ACTIVE: { bgcolor: "#96E9CB", color: "#056643", title: "Aktiv" },
   REVIEW_REQUIRED: {
