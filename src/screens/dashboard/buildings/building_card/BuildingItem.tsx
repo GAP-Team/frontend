790ba5e6713c264b18e1bfad361ac90b8b3c1bd7
SudiptoVE <sudipto@gap-pruefen.de>
2024-11-18 18:06:08 +0530

    Lint check

--- src/screens/dashboard/buildings/building_card/BuildingItem.tsx
+++ src/screens/dashboard/buildings/building_card/BuildingItem.tsx
@@ -27,7 +27,7 @@ const BuildingItem: React.FC<BuildingItemProps> = ({ building }) => {
     getFacilityTendersCount();
   }, []);
 
-  const getFacilityTendersCount = async () => {
+  const getFacilityTendersCount = async (): Promise<void> => {
     var count = 0;
     await Promise.all(
       building?.facilities?.map(async (facilityId: string) => {
