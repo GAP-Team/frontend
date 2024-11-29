60522ff5eaf3d1e7acbed962c9a0ed83c136230f
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-11-29 02:10:22 +0100

    style: formated TenderCard and NewTender files

--- src/screens/dashboard/tenders/add_tender_form/NewTender.tsx
+++ src/screens/dashboard/tenders/add_tender_form/NewTender.tsx
@@ -97,7 +97,8 @@ const NewTender = (): JSX.Element => {
       building: buildingObj,
       facility: facilityObj,
       toDate: values?.toDate,
-      urgency: values?.urgency === "Dringend" ? Urgency.URGENT : values?.urgency,
+      urgency:
+        values?.urgency === "Dringend" ? Urgency.URGENT : values?.urgency,
       fromDate: values?.fromDate,
       clientName: values?.clientName,
       tenderForm: values?.tenderForm,
