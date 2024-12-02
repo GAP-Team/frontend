51f9995c55ded5fa151ba1292f2e3cf5a2811524
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2024-12-02 21:36:09 +0100

    style: formated page.tsx files

--- src/app/real_estate/buildings/edit/[id]/page.tsx
+++ src/app/real_estate/buildings/edit/[id]/page.tsx
@@ -5,7 +5,5 @@ import NewBuilding from "@/screens/dashboard/buildings/add_building_form/NewBuil
 export default function UpdatedBuilding(): JSX.Element {
   const params = useParams();
   const id = Array.isArray(params.id) ? params.id[0] : params.id;
-  return (
-    <NewBuilding id={id} />
-  );
+  return <NewBuilding id={id} />;
 }
--- src/app/real_estate/tenders/edit/[id]/page.tsx
+++ src/app/real_estate/tenders/edit/[id]/page.tsx
@@ -3,7 +3,5 @@
 import NewTender from "@/screens/dashboard/tenders/add_tender_form/NewTender";
 
 export default function TenderOverview(): JSX.Element {
-  return (
-    <NewTender />
-  );
+  return <NewTender />;
 }
