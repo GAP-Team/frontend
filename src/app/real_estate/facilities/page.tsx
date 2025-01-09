33418b769344d18ab2934a80c98f8d2bde8a8d27
rihabalyasiri <rihab.al-yasiri@web.de>
2025-01-09 18:31:12 +0100

    refactor: make the header reusable for tenders and facilities

--- src/app/real_estate/facilities/page.tsx
+++ src/app/real_estate/facilities/page.tsx
@@ -1,4 +1,4 @@
-import Facilities from "@/screens/dashboard/facilities/facility_card/Facilities";
+import Facilities from "@/screens/dashboard/facilities/FacilityOverview";
 
 export default function FacilitiesPage(): JSX.Element {
   return <Facilities />;
