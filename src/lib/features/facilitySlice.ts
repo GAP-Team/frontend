84837b1dd2c0a3a65f11eb64b558bfd723709333
rihabalyasiri <rihab.al-yasiri@web.de>
2025-01-10 10:12:53 +0100

    refactor: delete not used in facility slice

--- src/lib/features/facilitySlice.ts
+++ src/lib/features/facilitySlice.ts
@@ -1,7 +1,6 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { Facility } from "@/screens/dashboard/facilities/facility_card/types";
-import buildingAPIs from "@/api/building";
 
 interface FacilityState {
   facilities: Facility[];
@@ -15,14 +14,6 @@ const initialState: FacilityState = {
   error: null as string | null,
 };
 
-export const fetchFacilities = createAsyncThunk(
-  "facility/fetchFacilities",
-  async (buildingId: string) => {
-    const response = await buildingAPIs.getBuildingFacilities(buildingId);
-    return response.data;
-  }
-);
-
 const FacilitySlice = createSlice({
   name: "facility",
   initialState,
@@ -30,20 +21,6 @@ const FacilitySlice = createSlice({
     setFacilities: (state, action: PayloadAction<Facility[]>) => {
       state.facilities = action.payload;
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchFacilities.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(fetchFacilities.fulfilled, (state, action) => {
-        state.facilities = action.payload;
-        state.loading = false;
-      })
-      .addCase(fetchFacilities.rejected, (state, action) => {
-        state.error = action.error.message || "Failed to fetch facilities";
-        state.loading = false;
-      });
   },
 });
 
