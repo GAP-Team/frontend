6c96eee7418b1a71786ba5ceed6e2aed84ee25ec
rihabalyasiri <rihab.al-yasiri@web.de>
2025-01-11 10:45:21 +0100

    refactor: rename components

--- src/app/real_estate/facilities/page.tsx
+++ src/app/real_estate/facilities/page.tsx
@@ -1,5 +1,5 @@
-import Facilities from "@/screens/dashboard/facilities/FacilityOverview";
+import FacilityOverview from "@/screens/dashboard/facilities/FacilityOverview";
 
 export default function FacilitiesPage(): JSX.Element {
-  return <Facilities />;
+  return <FacilityOverview />;
 }
--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -12,7 +12,7 @@ import NewTender from "./tenders/add_tender_form/NewTender";
 import RealEstateUser from "./real_estate_user/RealEstateUser";
 import NewBuilding from "./buildings/add_building_form/NewBuilding";
 import { SidebarItem, SubItem } from "@/components/navigation/GSidebar/SideBar";
-import Facilities from "./facilities/FacilityOverview";
+import FacilityOverview from "./facilities/FacilityOverview";
 import NewFacility from "./facilities/add_facility_form/NewFacility";
 
 interface DashboardProps {
@@ -56,7 +56,7 @@ const Dashboard: React.FC<DashboardProps> = ({ overrideComponent }) => {
           id: 20,
           text: "Alle Anlagen",
           url: "facilities",
-          component: <Facilities />,
+          component: <FacilityOverview />,
         },
         {
           id: 21,
