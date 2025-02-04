727b7d1486073ed02a0f92e81c17b456996abacd
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2025-02-04 05:15:22 +0100

    refact: changed the validation for the firstname and lastname feilds to accept international naming using regex. Updated registration page and user profile page

--- src/utils/ValidationSchema.ts
+++ src/utils/ValidationSchema.ts
@@ -3,6 +3,8 @@ import * as yup from "yup";
 const EMAIL_REGEX =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const NAME_REGEX = /^[\p{L}\s'-]+$/u;
+
 export const loginValidationSchema = yup.object({
   email: yup
     .string()
@@ -13,8 +15,18 @@ export const loginValidationSchema = yup.object({
 
 export const registrationValidationSchema = yup
   .object({
-    firstName: yup.string().required("Vorname ist erforderlich"),
-    lastName: yup.string().required("Nachname ist erforderlich"),
+    firstName: yup
+      .string()
+      .required("Vorname ist erforderlich")
+      .min(2, "Vorname muss mindestens 2 Zeichen lang sein")
+      .max(50, "Vorname darf maximal 50 Zeichen lang sein")
+      .matches(NAME_REGEX, "Name darf nur Buchstaben, Leerzeichen, Bindestriche und Apostrophe enthalten"),
+    lastName: yup
+      .string()
+      .required("Nachname ist erforderlich")
+      .min(2, "Nachname muss mindestens 2 Zeichen lang sein")
+      .max(50, "Nachname darf maximal 50 Zeichen lang sein")
+      .matches(NAME_REGEX, "Name darf nur Buchstaben, Leerzeichen, Bindestriche und Apostrophe enthalten"),
     email: yup
       .string()
       .matches(EMAIL_REGEX, "Eingabe einer gültigen E-Mail")
