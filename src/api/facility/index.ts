6a79c51a37236d5bdda14d20e22b4716cec23d8e
Bakhtiar Hussain <65579326+bakhtiardev@users.noreply.github.com>
2025-02-04 05:23:19 +0100

    Update src/api/facility/index.ts
    Co-authored-by: Rihab Al-yasiri <46373127+rihabalyasiri@users.noreply.github.com>

--- src/api/facility/index.ts
+++ src/api/facility/index.ts
@@ -3,7 +3,7 @@ import api from "../axios";
 const facilityAPIs = {
   /* Facility Routes */
   create: (data: any): any => api.post("/facilities", data),
-  delete: (tenderId: string): any => api.delete(`/facilities/${tenderId}`),
+  delete: (facilityId: string): any => api.delete(`/facilities/${facilityId}`),
   getFacilityTenders: (facilityId: string): any =>
     api.get(`/facilities/${facilityId}/tenders`),
 };
