17ce6d61818fb6b45c10f220a2c89491e1e37fc0
SudiptoVE <sudipto@gap-pruefen.de>
2025-02-05 17:53:26 +0530

    Build: updating file path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Grid } from "@mui/material";
 import LandingPage from "@/screens/landing_page/LandingPage";
-import LandingPageLayout from "@/screens/landingpage_layout/LandingpageLayout";
+import LandingPageLayout from "@/screens/landingpage_layout/LandingPageLayout";
 
 export default function Home(): JSX.Element {
   return (
