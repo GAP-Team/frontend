65389d0f2957e861a6533edfe5646da36d84b731
Bakhtiar Hussain <65579326+bakhtiardev@users.noreply.github.com>
2025-02-07 00:03:47 +0100

    Update src/api/facility/index.ts
    Co-authored-by: Rihab Al-yasiri <46373127+rihabalyasiri@users.noreply.github.com>

--- src/api/facility/index.ts
+++ src/api/facility/index.ts
@@ -5,7 +5,7 @@ const facilityAPIs = {
   create: (data: any): any => api.post("/facilities", data),
   getFacilityTenders: (facilityId: string): any =>
     api.get(`/facilities/${facilityId}/tenders`),
-  update: (facilityId: string, facilityDate: any): any =>
+  update: (facilityId: string, facilityData: any): any =>
     api.put(`/facilities/${facilityId}`, facilityDate),
 };
 
