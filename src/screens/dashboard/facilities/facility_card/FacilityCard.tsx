3ffff2921dfbf3178cd9333c277a64b040171019
Bakhtiar Hussain <bakhtiarhussain123@gmail.com>
2025-02-13 02:14:35 +0100

    format: formated the message in Action Menu

--- src/screens/dashboard/facilities/facility_card/FacilityCard.tsx
+++ src/screens/dashboard/facilities/facility_card/FacilityCard.tsx
@@ -88,7 +88,7 @@ const FacilityCard: React.FC<FacilityCardProps> = ({ facility }) => {
           itemId={facility?.id}
           onEdit={(id) => router.push(`/real_estate/facility/edit/${id}`)}
           onDelete={handleDeleteFacility}
-          messege={`Sind Sie sicher, dass Sie dieses Element${noOfTenders ? ` und die zugehörigen ${noOfTenders} Ausschreibungen` : ''} löschen möchten?`}
+          messege={`Sind Sie sicher, dass Sie dieses Element${noOfTenders ? ` und die zugehörigen ${noOfTenders} Ausschreibungen` : ""} löschen möchten?`}
         />
       </Box>
       <SectionTitle
