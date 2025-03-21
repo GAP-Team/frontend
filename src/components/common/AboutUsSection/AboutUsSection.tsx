b4133a145c41bbc78826240f9e899ef6436b2cc8
SudiptoVE <sudipto@gap-pruefen.de>
2025-03-21 13:56:33 +0530

    test: switching other branch

---
+++ src/components/common/AboutUsSection/AboutUsSection.tsx
@@ -0,0 +1,11 @@
+const AboutUsSection = (): JSX.Element => {
+    return (
+        <>
+            <div>
+                <h1>About Us</h1>
+            </div>
+        </>
+    );
+}
+
+export default AboutUsSection;
