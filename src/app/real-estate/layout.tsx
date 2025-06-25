b3ceb0a0146b62670e220320aca313112d01a415
rihabalyasiri <rihab.al-yasiri@web.de>
2025-06-25 23:39:27 +0200

    refactor: rename in user layout state

--- src/app/real-estate/layout.tsx
+++ src/app/real-estate/layout.tsx
@@ -79,7 +79,7 @@ const sidebarItems: SidebarItem[] = [
 const RealStateUserLayout: React.FC<any> = ({ children }) => {
   const router = useRouter();
   const pathname = usePathname();
-  const [selected, setSelected] = useState<SidebarItem | SubItem>(
+  const [selectedSidebarItem, setSelectedSidebarItem] = useState<SidebarItem | SubItem>(
     sidebarItems[0]
   );
 
@@ -99,12 +99,12 @@ const RealStateUserLayout: React.FC<any> = ({ children }) => {
       }
       return sidebarItems[0];
     };
-    setSelected(matchSidebarItem());
+    setSelectedSidebarItem(matchSidebarItem());
   }, [pathname]);
 
   const handleRedirect = (item: SidebarItem | SubItem): void => {
     if (item.url) {
-      setSelected(item);
+      setSelectedSidebarItem(item);
       router.push(item.url);
     }
   };
@@ -114,7 +114,7 @@ const RealStateUserLayout: React.FC<any> = ({ children }) => {
       return (
         <Layout
           sidebarItems={sidebarItems}
-          selected={selected}
+          selected={selectedSidebarItem}
           setSelected={handleRedirect}
         >
           <Box sx={{ width: "100%" }}>{children}</Box>
