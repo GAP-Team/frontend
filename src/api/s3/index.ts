733ddc69ac653c2e132a187e6d08119070d151ed
rihabalyasiri <rihab.al-yasiri@web.de>
2025-06-25 23:16:47 +0200

    refactor: update s3 api

--- src/api/s3/index.ts
+++ src/api/s3/index.ts
@@ -1,8 +1,8 @@
 import api from "../axios";
 
-const s3APIs = {
+const s3API = {
   /* S3 Routes */
-  upload: (data: any): any => api.post("/files", data),
+  upload: (file: any): any => api.post("/files", file),
   getFile: (key: string): any =>
     api.get(`/files/${key}`, {
       responseType: "blob",
@@ -10,4 +10,4 @@ const s3APIs = {
   delete: (key: any): any => api.delete(`/files/${key}`),
 };
 
-export default s3APIs;
+export default s3API;
