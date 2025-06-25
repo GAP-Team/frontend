edbeaf725919fed4e00e62c2a4c5ec89e4c33379
rihabalyasiri <rihab.al-yasiri@web.de>
2025-06-25 23:28:08 +0200

    fix: update to about us name

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,7 +3,7 @@ import { Grid } from "@mui/material";
 import LandingPageLayout from "@/screens/landingpage_layout/LandingPageLayout";
 import AboutUsSection from "@/components/common/AboutUsSection/AboutUsSection";
 
-export default function ContactUs(): JSX.Element {
+export default function AboutUs(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center bg-white justify-between">
       <LandingPageLayout>
