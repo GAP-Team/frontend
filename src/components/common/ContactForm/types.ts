b22981ed1c6dc4956dac9d17fe213dcb03c4cd0d
rihabalyasiri <rihab.al-yasiri@web.de>
2025-06-26 21:24:47 +0200

    refactor: move the components

--- src/components/common/ContactForm/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ContactFormProps {
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
-  email: string;
-  subject: string;
-  message: string;
-  dataPrivacyAccepted: boolean;
-}
