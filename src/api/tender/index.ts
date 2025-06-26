b1eb021d20dedf738bca3ec325e03f22945ce795
rihabalyasiri <rihab.al-yasiri@web.de>
2025-06-26 17:28:58 +0200

    fix: replace any with its real data types

--- src/api/tender/index.ts
+++ src/api/tender/index.ts
@@ -1,11 +1,12 @@
+import { Tender } from "@/screens/dashboard/tenders/tender_card/types";
 import api from "../axios";
 
 const tenderAPI = {
   /* Tender Routes */
   // FIXME: Add proper types for the parameters and response
-  create: (tender: any): any => api.post("/tenders", tender),
+  create: (tender: Tender): Promise<{id: string}> => api.post("/tenders", tender),
   delete: (tenderId: string): any => api.delete(`/tenders/${tenderId}`),
-  update: (tenderId: string, tender: any): any =>
+  update: (tenderId: string, tender: Partial<Tender>): Promise<Tender> =>
     api.put(`/tenders/${tenderId}`, tender),
 };
 
