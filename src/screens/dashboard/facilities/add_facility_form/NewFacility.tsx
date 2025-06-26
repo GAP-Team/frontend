d2b2754f4fbb4cf9abb3cb501976121f95ffb34b
rihabalyasiri <rihab.al-yasiri@web.de>
2025-06-26 10:28:12 +0200

    fix: rename federalstate

--- src/screens/dashboard/facilities/add_facility_form/NewFacility.tsx
+++ src/screens/dashboard/facilities/add_facility_form/NewFacility.tsx
@@ -81,7 +81,7 @@ const NewFacility: React.FC<NewFacilityProps> = ({
     const query = {
       userId: user?.id,
       city: "",
-      federalState: "",
+      state: "",
       facilityType: "",
     };
     dispatch(fetchBuildings(query));
