76671a629691dd7cbda44be2de02f1bfc7f01364
rihabalyasiri <rihab.al-yasiri@web.de>
2025-07-03 14:25:22 +0200

    fix: conflict wrong merge

--- src/components/common/ContractDetails/ContractDetails.tsx
+++ src/components/common/ContractDetails/ContractDetails.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect } from "react";
 import NoAccessPage from "../NoAccessPage";
-import { checkIsLoggedIn } from "@/utils/helperJWT";
+import { checkIsLoggedIn } from "@/utils/auth";
 import { USER_ROLE, DOCUMENT_TYPE } from "@/utils/enums";
 import { showSnackbar } from "@/components/root-snackbar";
 import { Box, Grid, Paper, Typography } from "@mui/material";
@@ -19,7 +19,6 @@ const ContractDetails: React.FC<ContractDetailsProps> = ({
 }): JSX.Element => {
   const appDispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
-  const contractDetails = useAppSelector(getContract);
   const contractDetails = useAppSelector(getContract);
 
   useEffect(() => {
