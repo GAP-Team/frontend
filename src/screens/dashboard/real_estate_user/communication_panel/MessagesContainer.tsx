39007c3a23a95c4476570cab53b70899fa27c520
rihabalyasiri <rihab.al-yasiri@web.de>
2025-07-03 14:37:13 +0200

    refactor: rename props of the messages

--- src/screens/dashboard/real_estate_user/communication_panel/MessagesContainer.tsx
+++ src/screens/dashboard/real_estate_user/communication_panel/MessagesContainer.tsx
@@ -1,7 +1,7 @@
 // The list component now uses JobApplicationItem for each job application.
 import React from "react";
 import { List } from "@mui/material";
-import { NewsItemProps as MessageItemProps } from "./types";
+import {MessageItemProps } from "./types";
 import Message from "./Message";
 import { scrollBarStyles } from "@/components/scrollbar/Scrollbar";
 
