e68a0a5151df5f1af39ee18e76e5354ea0a7bb79
SudiptoVE <sudipto@gap-pruefen.de>
2025-07-03 12:17:58 +0530

    Pref: showing facilityType - subcategory - facilityName in add Tender form

--- src/screens/dashboard/tenders/add_tender_form/TenderBuilding.tsx
+++ src/screens/dashboard/tenders/add_tender_form/TenderBuilding.tsx
@@ -4,14 +4,15 @@ import Grid from "@mui/material/Grid";
 import { useSelector } from "react-redux";
 import { useFormikContext } from "formik";
 import buildingAPIs from "@/api/building";
-import { HELP_ICON_BUTTON_COLOR, Item } from "@/utils/Constants";
 import { useEffect, useState } from "react";
 import { AddTenderFormValues } from "./types";
+import HelpIcon from "@/components/button/HelpIcon";
 import { FormControl, MenuItem, Select } from "@mui/material";
 import CustomSelect from "@/components/drop_down/CustomSelect";
 import { getUserBuildings } from "@/lib/features/buildingSlice";
-import HelpIcon from "@/components/button/HelpIcon";
+import { HELP_ICON_BUTTON_COLOR, Item } from "@/utils/Constants";
 import LabelWithAsterisk from "@/components/label/LabelWithAsterisk";
+import { Facility } from "@/screens/dashboard/facilities/facility_card/types";
 import { AddFacilityFormValues } from "../../facilities/add_facility_form/types";
 
 const TenderBuilding = (): JSX.Element => {
@@ -47,9 +48,9 @@ const TenderBuilding = (): JSX.Element => {
     );
 
     const facilityOptions: Item[] = [];
-    allFacilities?.data?.forEach((facility: any) => {
+    allFacilities?.data?.forEach((facility: Facility) => {
       const temp = {
-        label: facility?.name,
+        label: `${facility?.facilityType} - ${facility?.subcategory} - ${facility?.name}`,
         value: facility?.id,
       };
       facilityOptions.push(temp);
@@ -71,9 +72,9 @@ const TenderBuilding = (): JSX.Element => {
         await buildingAPIs.getBuildingFacilities(selectedBuildingId);
 
       const facilityOptions: Item[] = [];
-      allFacilities?.data?.map((facility: any) => {
+      allFacilities?.data?.map((facility: Facility) => {
         const temp = {
-          label: facility?.name,
+          label: `${facility?.facilityType} - ${facility?.subcategory} - ${facility?.name}`,
           value: facility?.id,
         };
         facilityOptions.push(temp);
