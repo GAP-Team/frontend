34edd7439b02adc3e0f653a7a97b11f320960d6a
SudiptoVE <sudipto@gap-pruefen.de>
2025-07-07 17:02:33 +0530

    Refactoring the components in the proper folder

--- src/screens/contract_application/ContractApplicationForm.tsx
+++ src/screens/contract_application/ContractApplicationForm.tsx
@@ -1,4 +1,4 @@
-import GButton from "../button/GButton";
+import GButton from "../../components/button/GButton";
 import { ROUTES } from "@/utils/routes";
 import { useFormikContext } from "formik";
 import { useRouter } from "next/navigation";
--- src/screens/contract_application/ContractServicesForm.tsx
+++ src/screens/contract_application/ContractServicesForm.tsx
@@ -9,9 +9,9 @@ import {
   Typography,
 } from "@mui/material";
 import { useState } from "react";
-import UploadButton from "../button/UploadButton";
+import UploadButton from "../../components/button/UploadButton";
 import { previousAdvantages } from "@/utils/Constants";
-import LabelWithAsterisk from "../label/LabelWithAsterisk";
+import LabelWithAsterisk from "../../components/label/LabelWithAsterisk";
 
 const ContractServicesForm = ({ formik }: { formik?: any }): JSX.Element => {
   const [newAdvantage, setNewAdvantage] = useState<string>("");
