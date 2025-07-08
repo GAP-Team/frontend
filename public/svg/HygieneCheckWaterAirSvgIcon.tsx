0f3b776dbcd38c1383bf61bac87e500171f79dd2
rihabalyasiri <rihab.al-yasiri@web.de>
2025-07-08 12:55:28 +0200

    fix: be consistent in design in icon

--- public/svg/HygieneCheckWaterAirSvgIcon.tsx
+++ public/svg/HygieneCheckWaterAirSvgIcon.tsx
@@ -1,7 +1,7 @@
 export const HygieneCheckWaterAirSvgIcon = ({ className }: { className?: string }): JSX.Element => {
   return (
     <svg
-       className={`${className || "h-12 w-12 text-blue-500"}`}
+       className={`${className || "w-14 h-14 text-blue-600"}`}
       viewBox="0 0 64 64"
       xmlns="http://www.w3.org/2000/svg"
     >
