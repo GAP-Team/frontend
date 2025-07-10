4d8fee07c0a005425a6b71d1619dfbe94f614942
rihabalyasiri <rihab.al-yasiri@web.de>
2025-07-10 16:31:48 +0200

    style: fix format & lint

--- src/screens/real_estate_owner/dashboard/communication_panel/ChatPanel.tsx
+++ src/screens/real_estate_owner/dashboard/communication_panel/ChatPanel.tsx
@@ -4,7 +4,6 @@ import ScrollableLayout from "../../../../components/layout/ScrollableLayout";
 import { messages, DashboardComponentsProps } from "@/utils/Constants";
 import MessagesContainer from "./MessagesContainer";
 
-
 const ChatPanel: React.FC<DashboardComponentsProps> = (): JSX.Element => {
   return (
     <ScrollableLayout>
--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,9 +1,8 @@
-import { FilterOptionType } from "@/typings/types";
+import { FilterOptionType, Notification } from "@/typings/types";
 import { USER_ROLE } from "./enums";
 import HomeWorkIcon from "@mui/icons-material/HomeWork";
 import OfficeImage from "../../public/images/office.jpg";
 import DescriptionIcon from "@mui/icons-material/Description";
-import { Notification } from "@/typings/types";
 import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
 
