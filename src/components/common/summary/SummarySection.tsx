a9bea2d12a8b25283287297d44d6ce198d567609
rihabalyasiri <rihab.al-yasiri@web.de>
2025-07-11 15:01:29 +0200

    refactor: using @ import

--- src/components/common/summary/SummarySection.tsx
+++ src/components/common/summary/SummarySection.tsx
@@ -4,7 +4,7 @@ import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import SummaryLayout from "@/components/layout/SummaryLayout";
 import { FaRegEdit } from "react-icons/fa";
-import DetailItem from "../../data-display/DetailItem";
+import DetailItem from "@/components/data-display/DetailItem";
 export interface Detail {
   label: string;
   value: string;
