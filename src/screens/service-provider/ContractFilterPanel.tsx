0db53831a53b8580562d8ce70620cf6cc4d56d82
SudiptoVE <sudipto@gap-pruefen.de>
2025-09-01 11:25:37 +0530

    Pref: Button posution beside each other

--- src/screens/service-provider/ContractFilterPanel.tsx
+++ src/screens/service-provider/ContractFilterPanel.tsx
@@ -79,7 +79,10 @@ const ContractFilterPanel: React.FC<SideFilterPanelProps> = ({
           <Typography variant="h6" fontWeight="bold">
             Filters
           </Typography>
-          <GButton type="submit">Filter</GButton>
+          <div>
+            <GButton type="submit">Filter</GButton>
+            <GButton onClick={handleOnReset}>Klar</GButton>
+          </div>
         </div>
         <SideFilterPanelOptions
           options={listOfGermanStates}
@@ -99,7 +102,6 @@ const ContractFilterPanel: React.FC<SideFilterPanelProps> = ({
           title={FilterPanelLabels.TENDER_TYPE}
           preSelectedOptions={formik.values.tenderTypes}
         />
-        <GButton onClick={handleOnReset}>Filter löschen</GButton>
       </form>
     </Box>
   );
