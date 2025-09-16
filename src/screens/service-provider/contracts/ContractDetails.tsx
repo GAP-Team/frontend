01c2660f4ab4ffd2057a801121e891505c252700
MariaAmeer1998 <maria@gap-pruefen.de>
2025-09-16 14:14:05 +0200

    Update src/screens/service-provider/contracts/ContractDetails.tsx
    Co-authored-by: Rihab Al-yasiri <46373127+rihabalyasiri@users.noreply.github.com>

--- src/screens/service-provider/contracts/ContractDetails.tsx
+++ src/screens/service-provider/contracts/ContractDetails.tsx
@@ -8,7 +8,7 @@ import ContractSummarySection from "./ContractSummarySection";
 import { fetchContractById, getContract } from "@/lib/features/contractSlice";
 import TenderTitleBar from "@/screens/real-estate-owner/tenders/tender-overview/TenderTitleBar";
 import DocumentList from "@/screens/real-estate-owner/buildings/building-overview/DocumentList";
-import NoContentPage from "@/components/common/pages/FallbackPage";
+import FallbackPage from "@/components/common/pages/FallbackPage";
 import { ROUTES } from "@/utils/routes";
 import NoAccessImage from "@images/no_access.png";
 
