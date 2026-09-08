b6822c8761d50b0d75f1772a81ec7774fb4f512c
Rihab Al-Yasiri <rihab.al-yasiri@adesso.de>
2026-09-08 14:36:57 +0200

    fix bug isUserAdmin

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -2,14 +2,14 @@
 import { useEffect } from "react";
 import { ROUTES } from "@/utils/routes";
 import { useRouter } from "next/navigation";
-import { userIsAdmin, checkIsLoggedIn } from "@/utils/auth";
+import { isUserAdmin, checkIsLoggedIn } from "@/utils/auth";
 import UsersOverview from "@/screens/admin/users/UsersOverview";
 import NoAccessImage from "@images/no_access.png";
 import FallbackPage from "@/components/common/pages/FallbackPage";
 
 export default function UsersPage(): JSX.Element {
   const router = useRouter();
-  const isAdmin = userIsAdmin();
+  const isAdmin = isUserAdmin();
   const isLoggedIn = checkIsLoggedIn();
 
   useEffect(() => {
